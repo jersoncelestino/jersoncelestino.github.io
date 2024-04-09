dffd3cb40034e92922d82d728101b6e51eb64170
Jerson <jerson.celestino@hotmail.com>
2024-04-09 13:43:23 -0500

    Commit inicial

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template #De esta manera llamamos al framework #El render template nos ayuda a importar un html
+app = Flask(__name__) #Guardamos en una variable lo que nos devuelve el método para usarlo para nuestras urls y servidor
+
+#RUTA PARA MANEJAR LA PÁGINA PRINCIPAL
+@app.route('/')#Aqui estamos creando una ruta pra nuestra pagina principal
+def home():
+    return render_template('home.html')#usamos el render_template para llamar a un html
+
+#RUTA PARA MANEJAR LA PÁGINA ABOUT
+@app.route('/about')#Aqui estamos creando una ruta pra nuestra pagina about
+def about():
+    return render_template('about.html')#usamos el render_template para llamar a un html
+
+
+#Necesitamos saber si nuestra aplicación está escuchando constantemente
+if __name__ == '__main__': #Esta validación nos ayuda a saber si estamos ejecutando el archivo principal
+    app.run(debug=True)#Este metodo run nos permite eejcutar nuestra aplicación #Debug=true es para indicar al código que estaré modificando constantemente
+
+
+
+
+
+
