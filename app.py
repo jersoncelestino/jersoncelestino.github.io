5a968fe987be599faa0050a7b63aec92d85a8dc4
Jerson <jerson.celestino@hotmail.com>
2024-04-09 15:56:02 -0500

    Commit app config

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template #De esta manera llamamos al framework #El render template nos ayuda a importar un html
 app = Flask(__name__) #Guardamos en una variable lo que nos devuelve el método para usarlo para nuestras urls y servidor
 
+
+app.config['SERVER_NAME'] = 'localhost:5000'  # Cambia esto por el nombre de dominio y el puerto de tu aplicación
+app.config['APPLICATION_ROOT'] = '/'  # Esto es opcional, pero puedes configurarlo según sea necesario
+app.config['PREFERRED_URL_SCHEME'] = 'https'  # Cambia esto si tu aplicación usa HTTPS
+
+
 #RUTA PARA MANEJAR LA PÁGINA PRINCIPAL
 @app.route('/')#Aqui estamos creando una ruta pra nuestra pagina principal
 def home():
